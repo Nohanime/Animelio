25f549553fd0b95699e9c01acc016e1cf06d8964
Nohanime <152039993+Nohanime@users.noreply.github.com>
2023-11-28 23:57:06 +0100

    Add files via upload

---
+++ VOSTFR/VOSTFR.js
@@ -0,0 +1,8 @@
+function search() {
+    // Récupérer la valeur de la barre de recherche
+    var searchTerm = document.getElementById('search-input').value;
+
+    // Vous pouvez effectuer des opérations de recherche ici et afficher les résultats
+    // Pour l'exemple, nous allons simplement afficher une alerte avec le terme recherché
+    alert('Recherche en cours pour : ' + searchTerm);
+}
